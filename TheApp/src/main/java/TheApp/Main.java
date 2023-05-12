2e69650f461a1c6ee3420f3c9b2d974738afb791
Marek Podolak <podolakmarek9119@gmail.com>
2023-05-12 04:29:04 +0200

    first update4j config file

--- TheApp/src/main/java/TheApp/Main.java
+++ TheApp/src/main/java/TheApp/Main.java
@@ -3,12 +3,19 @@ package TheApp;
 
 import Update.ConfigCreator;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         ConfigCreator confCreator = new ConfigCreator();
-        confCreator.createConfig();
         System.out.println("Hello world!");
         System.out.println(System.getProperty("user.dir"));
+        confCreator.showPath();
+        try {
+            confCreator.createConfig();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- Update/src/main/java/Update/ConfigCreator.java
+++ Update/src/main/java/Update/ConfigCreator.java
@@ -1,14 +1,29 @@
 package Update;
 
 import org.update4j.Configuration;
+import org.update4j.FileMetadata;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ConfigCreator {
     private String appDir = System.getProperty("user.dir");
+    private String theAppTarget = appDir + "\\TheApp\\target";
     private  String bootstrapTarget = appDir + "\\Bootstrap\\target";
-    public void createConfig()
-    {
-        Configuration config = Configuration.builder()
 
+    public void showPath()
+    {
+        System.out.println(theAppTarget + "\\TheApp-1.0-SNAPSHOT.jar");
+    }
+    public void createConfig() throws IOException {
+        Configuration config = Configuration.builder()
+                .baseUri("https://github.com/Skurczybyk/UpdateTest/tree/master/TheApp/target")
+                .basePath(theAppTarget)
+                .file(FileMetadata.readFrom(theAppTarget + "\\TheApp-1.0-SNAPSHOT.jar"))
                 .build();
+        try (Writer out = Files.newBufferedWriter(Paths.get(bootstrapTarget + "\\config.xml")))
+        {config.write(out);}
     }
 }
